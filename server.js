34ecc60954d67f6454a14e16d4daa2ab16b88932
alykhan2212 <alykhan2212@gmail.com>
2021-03-25 03:09:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 //Environment variables
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.SERVE_PORT || 5000;
 const uri = process.env.DB_URI;
 
 if (process.env.NODE_ENV === 'production') {
