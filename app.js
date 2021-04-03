e40424d6a0544167b956055d6c705808a063c841
alykhan2212 <alykhan2212@gmail.com>
2021-04-03 22:19:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -22,20 +22,20 @@ app.use('/api/users', usersRoute);
 app.use('/api/posts', postsRoute);
 
 //Error handling
-app.use((req, res, next) => {
-    const error = new Error('Not Founddsd');
-    error.status = 404;
-    next(error);
-})
+// app.use((req, res, next) => {
+//     const error = new Error('Not Found');
+//     error.status = 404;
+//     next(error);
+// })
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message: error.message
-        }
-    });
-})
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+//     res.json({
+//         error:{
+//             message: error.message
+//         }
+//     });
+// })
 
 //build client on production
 if (process.env.NODE_ENV === 'production') {
