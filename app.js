5d76dc0431df14b6381f73daac34569b6d0504db
alykhan2212 <alykhan2212@gmail.com>
2021-04-07 23:14:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -21,12 +21,6 @@ const postsRoute = require('./routers/posts');
 app.use('/api/users', usersRoute);
 app.use('/api/posts', postsRoute);
 
-//Error handling
-// app.use((req, res, next) => {
-//     const error = new Error('Not Found');
-//     error.status = 404;
-//     next(error);
-// })
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
