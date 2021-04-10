c478d7661bc2ae633a9db1533bb39ca86956f407
alykhan2212 <alykhan2212@gmail.com>
2021-04-10 23:24:46 +0500

    refresh error

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.use((error, req, res, next) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build')));
     
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+      });
 }
 
 module.exports = app;
